dc5932e3e224dfd2668527f88d878ebbabec55d1
irfanzakhir <irfanzakheer@gmail.com>
2026-07-08 22:18:23 +0530

    initial commit

---
+++ src/main/java/com/clarity/clarity/Clarity.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.clarity.clarity;
+
+/**
+ *
+ * @author Ann Maria
+ */
+public class Clarity {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/clarity/clarity/Datas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.clarity.clarity;
+
+/**
+ *
+ * @author irfan
+ */
+public class Datas {
+    public static String st_uname_sp_get ="";
+    public static String get_Year ="";
+    public static String loggedInFacultyName = "";
+    public static String selectedSessionId;
+    public static String selectedQueryText;
+    public static String loggedInStudentId;
+    public static String loggedInStudentYear;
+
+}
